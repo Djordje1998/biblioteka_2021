2c9692d12c0908a43bcc95d3766782a2977d545d
Djordje1998 <djordjen7@gmail.com>
2021-03-15 15:46:49 +0100

    Dokumentovana klasa Biblioteka

--- src/biblioteka/Biblioteka.java
+++ src/biblioteka/Biblioteka.java
@@ -4,8 +4,16 @@ import java.util.LinkedList;
 
 import biblioteka.interfejs.BibliotekaInterface;
 
+/**
+ * Ova klasa implementira interface BibliotekaInterface i predstavlja implementaciju preko liste.
+ * @author Djordje Novakovic
+ *
+ */
 public class Biblioteka implements BibliotekaInterface {
 
+	/**
+	 * Lista sa svim knjigama u biblioteci.
+	 */
 	private LinkedList<Knjiga> knjige = new LinkedList<>();
 	
 	@Override
