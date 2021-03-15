62a6bf9230abea7748ad034e6938bd68aee62c9a
Djordje1998 <djordjen7@gmail.com>
2021-03-15 15:43:47 +0100

    Dokumentovan interface BibliotekaInterface.

--- src/biblioteka/interfejs/BibliotekaInterface.java
+++ src/biblioteka/interfejs/BibliotekaInterface.java
@@ -5,10 +5,23 @@ import java.util.LinkedList;
 import biblioteka.Autor;
 import biblioteka.Knjiga;
 
+/**
+ * Interface koji predstavlja biblioteku.
+ * 
+ * @author Djordje Novakovic
+ *
+ */
 public interface BibliotekaInterface {
 
+	/**
+	 * Dodaje novu knjigu u biblioteku.
+	 * 
+	 * @param knjiga Knjiga koja se dodaje
+	 * @throws java.lang.NullPointerException ako je uneta knjiga null
+	 * @throws java.lang.RuntimeException ako je uneta knjiga vec postoji u biblioteci (duplikat)
+	 */
 	public void dodajKnjigu(Knjiga knjiga);
-	
+
 	public void obrisiKnjigu(Knjiga knjiga);
 	
 	public LinkedList<Knjiga> vratiSveKnjige();
